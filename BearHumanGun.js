f3957e7924bd8f0484bc229e539917c1e24d786d
Leire Larrea <larrea.leire@gmail.com>
2021-06-22 22:29:24 +0100

    run on browser only
    added comment to run on browser

--- BearHumanGun.js
+++ BearHumanGun.js
@@ -1,6 +1,9 @@
+// This will only run in a browser about:blank >> devTools (F12)
+// because prompt() is not part of JS but a method for "Window" which is not defined in Node.js
+
 function getUserChoice(userInput){
     userInput = userInput.toLowerCase();
-    console.log('the user input is ' + userInput);
+    console.log('You chose: ' + userInput);
     if(userInput == 'bear'|| userInput == 'human'|| userInput == 'gun'){
         return userInput;
     } else {
@@ -11,38 +14,32 @@ function getUserChoice(userInput){
 var choices = ['bear', 'human', 'gun']
 function getComputerChoice(){
     computerInput = Math.floor(Math.random() * choices.length);
-    console.log('computer choose '+choices[computerInput]);
+    console.log('The computer chose: '+choices[computerInput]);
     return choices[computerInput];
 }
 
 function getResult(userInput, computerChoice){
     var userChoice = getUserChoice(userInput);
     var computerChoice = getComputerChoice();
-    if (userChoice == 'bear'){
+    if (userChoice == computerChoice)
+        console.log('It is a draw');
+    else if (userChoice == 'bear') {
         if (computerChoice == 'human')
                 console.log('user wins ' + userChoice + ' VS ' + computerChoice);
-         else if (computerChoice == 'gun')
+        else if (computerChoice == 'gun')
                 console.log('computer wins ' + userChoice + ' VS ' + computerChoice);
-         else     
-                console.log('It is a draw');
     }
-    if (userChoice == 'human'){
+    else if (userChoice == 'human') {
         if (computerChoice == 'bear')
                 console.log('computer wins ' + userChoice + ' VS ' + computerChoice);
         else if (computerChoice == 'gun')
                 console.log('user wins ' + userChoice + ' VS ' + computerChoice);
-        else     
-                console.log('It is a draw');
     }
-    
-    if (userChoice == 'gun'){
+    else
       if (computerChoice == 'bear')
             console.log('user wins ' + userChoice + ' VS ' + computerChoice);
       else if (computerChoice == 'human')
             console.log('computer wins ' + userChoice + ' VS ' + computerChoice);
-      else     
-                console.log('It is a draw');
-    }
 }
 
 var finalChoice = prompt('select');
